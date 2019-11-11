3939f2a5de1c6ce9f373a3909e12cc9f341da03b
anhishek <abishek.xetri100@gmail.com>
2019-11-11 19:13:45 +0545

    intro

---
+++ third.py
@@ -0,0 +1,10 @@
+print("h\ne\nl\nl\no")
+a=5
+b=10
+print(a+b)
+print("foo" * 3)
+print("hello" + "world")
+first ='abishek'
+adress="biratchowk"
+age="22"
+print("my name is "+first+' and adress is',+adress+ 'my age is'+age)
