e56b0ff09f629b781f801657f2f369d49c923ac4
anhishek <abishek.xetri100@gmail.com>
2019-11-17 20:23:57 +0545

    .

---
+++ sum.py
@@ -0,0 +1,14 @@
+science=int(input("enter the marks of science"))
+social=int(input("enter marks of social"))
+nepali=int(input("enter marks of nepali"))
+math=int(input("enter marks of math"))
+computer=int(input("enter marks of computer"))
+sum1=[]
+sum1.append(science)
+
+sum1.append(social)
+sum1.append(nepali)
+sum1.append(math)
+sum1.append(computer)
+print(sum(sum1))
+
